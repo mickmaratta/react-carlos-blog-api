4223b8b371b00b1c61ba9fae3301117ba47998e9
Mick Maratta <mickmaratta@gmail.com>
2022-10-14 17:13:20 -0400

    Finished Draft 1

--- controllers/post.js
+++ controllers/post.js
@@ -1,6 +1,7 @@
 import { db } from "../db.js";
 import jwt from "jsonwebtoken"
 
+//GET ALL POSTS
 export const getPosts = (req, res) => {
   const q = req.query.cat
     ? "SELECT * FROM posts WHERE cat=?"
@@ -13,9 +14,10 @@ export const getPosts = (req, res) => {
   });
 };
 
+//GET SINGLE POST
 export const getPost = (req, res) => {
   const q =
-    "SELECT `username`, `title`, `desc`, p.img, u.img AS userImg, `cat`, `date` FROM users u JOIN posts p ON u.id=p.uid WHERE p.id = ?";
+    "SELECT p.id, `username`, `title`, `desc`, p.img, u.img AS userImg, `cat`, `date` FROM users u JOIN posts p ON u.id=p.uid WHERE p.id = ?";
 
     db.query(q, [req.params.id], (err, data) => {
         if(err) return res.status(500).json(err);
@@ -23,9 +25,35 @@ export const getPost = (req, res) => {
     })
 };
 
+
+//ADD POST
 export const addPost = (req, res) => {
-  res.json("from controller");
+  const token = req.cookies.access_token
+  if(!token) return res.status(401).json("Not authorized!")
+
+  jwt.verify(token, "jwtkey", (err, userInfo) => {
+    if(err) return res.status(403).json("Token is not valid!");
+
+    const q = "INSERT INTO posts(`title`, `desc`, `img`, `cat`, `date`, `uid`) VALUES (?)"
+
+    const values = [
+      req.body.title,
+      req.body.desc,
+      req.body.img,
+      req.body.cat,
+      req.body.date,
+      userInfo.id
+    ]
+
+    db.query(q,[values], (err, data) => {
+      if(err) return res.status(500).json(err)
+      return res.json("Post has been created.")
+    })
+  })
 };
+
+
+//DELETE POST
 export const deletePost = (req, res) => {
   const token = req.cookies.access_token
   if(!token) return res.status(401).json("Not authorized!")
@@ -43,6 +71,28 @@ export const deletePost = (req, res) => {
     })
   })
 };
+
+//UPDATE POST
 export const updatePost = (req, res) => {
-  res.json("from controller");
+  const token = req.cookies.access_token
+  if(!token) return res.status(401).json("Not authorized!")
+
+  jwt.verify(token, "jwtkey", (err, userInfo) => {
+    if(err) return res.status(403).json("Token is not valid!");
+
+    const postId = req.params.id;
+    const q =  "UPDATE posts SET `title`=?,`desc`=?,`img`=?,`cat`=? WHERE `id` = ? AND `uid` = ?"
+
+    const values = [
+      req.body.title,
+      req.body.desc,
+      req.body.img,
+      req.body.cat,
+    ]
+
+    db.query(q,[...values, postId, userInfo.id], (err, data) => {
+      if(err) return res.status(500).json(err)
+      return res.json("Post has been updated.")
+    })
+  })
 };
--- index.js
+++ index.js
@@ -3,13 +3,30 @@ import postRoutes from "./routes/posts.js";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import cors from "cors";
-import cookieParser from "cookie-parser"
+import cookieParser from "cookie-parser";
+import multer from "multer";
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, '../client/public/uploads')
+    },
+    filename: function (req, file, cb) {
+      cb(null, Date.now()+file.originalname)
+    }
+  })
+
+const upload = multer({ storage });
+
+app.post('/api/upload', upload.single('file'), function (req, res) {
+    const file = req.file;
+    res.status(200).json(file.filename)
+  });
 
 app.get("/test", (req, res) => {
     res.json("It works!")
@@ -18,6 +35,7 @@ app.get("/test", (req, res) => {
 app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/posts", postRoutes)
+app.use("/api/upload", postRoutes)
 
 app.listen(8800, ()=> {
     console.log("Connected!")
